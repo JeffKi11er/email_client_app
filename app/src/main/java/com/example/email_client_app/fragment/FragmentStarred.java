bca2ac5da0c7a0b7a87f366300365fe2d093de12
pcranger <52710878+pcranger@users.noreply.github.com>
2020-12-21 15:48:55 +0700

    create fragment layout starred and snoozed

--- app/src/main/java/com/example/email_client_app/fragment/FragmentStarred.java
+++ app/src/main/java/com/example/email_client_app/fragment/FragmentStarred.java
@@ -23,7 +23,7 @@ public class FragmentStarred extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_check,container,false);
+        View view = inflater.inflate(R.layout.fragment_layout_starred,container,false);
         return view;
     }
 
@@ -45,7 +45,7 @@ public class FragmentStarred extends Fragment {
                 "đã bảo là không có tiêu đề"));
         emails.add(new ItemEmail("Nguyen Cong Thanh","15/12/2020",R.drawable.cat,true,"Không tiêu đề",
                 "đã bảo là không có tiêu đề"));
-        rclEmails = getActivity().findViewById(R.id.rcl_emails);
+        rclEmails = getActivity().findViewById(R.id.rcl_starred);
         rclEmails.setAdapter(new AdapterStarred(getContext(),emails));
     }
 }
